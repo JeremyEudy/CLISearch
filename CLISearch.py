6793f476f32aa898ea38815783d595a27cabf6e1
Jeremy Eudy <jeremyeudy@gmail.com>
2019-02-18 11:42:24 -0500

    Initial Commit

---
+++ CLISearch.py
@@ -0,0 +1,34 @@
+#Author: Jeremy Eudy
+import json
+import requests
+import sys
+from pprint import pprint
+from urllib.parse import urlencode, quote_plus
+from py_expression_eval import Parser
+
+search = sys.argv
+parser = Parser()
+if(len(search) == 0):
+    print("Usage: search *search string*")
+else:
+    urlFront = "https://api.duckduckgo.com/?"
+    search = ''.join(search[1:])
+    params = {'q': search, 'o':'json'}
+    url = urlFront+urlencode(params, quote_via=quote_plus)
+    answer = json.loads(requests.get(url).text)
+    if(answer['Answer'] != '' and answer['Answer']['id'] == 'calculator'):
+        exp = str(parser.parse(search).evaluate({}))
+        print('{} = {}'.format(search, exp))
+
+    else:
+        print('Abstract URL:\n\t{}\n'.format(answer['AbstractURL']))
+        topics = answer['RelatedTopics']
+        text = list("")
+        urls = list("")
+        for i in range(0, 3):
+            text.append(topics[i]['Text'])
+            urls.append(topics[i]['FirstURL'])
+
+        print('Related topics:')
+        for i in range(0, 3):
+            print('\t{}\n\t{}\n'.format(text[i], urls[i]))
