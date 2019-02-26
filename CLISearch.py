b618fca5c8c0e9a8c0da63ec077182ffeb97ae3f
Jeremy Eudy <jeremyeudy@gmail.com>
2019-02-26 14:18:41 -0500

    Now gives a search link if there are no instant answers, and made it more accepting of errors.

--- CLISearch.py
+++ CLISearch.py
@@ -5,16 +5,29 @@ import sys
 from urllib.parse import urlencode, quote_plus
 from py_expression_eval import Parser
 
+
+#api url format: https://api.duckduckgo.com/?q=DuckDuckGo&format=json 
+#standard url format: https://duckduckgo.com/?q=test+search&atb=v1-1&ia=web
+
 search = sys.argv
 parser = Parser()
 if(len(search) == 0):
     print("Usage: search *search string*")
 else:
     urlFront = "https://api.duckduckgo.com/?"
-    search = ''.join(search[1:])
+    altUrlFront = "https://duckduckgo.com/?q="
+    altUrlBack = "&atb-v1-1&ia=web"
+    apiSearch = ''.join(search[1:])
+    stdSearch = '+'.join(search[1:])
     params = {'q': search, 'o':'json'}
     url = urlFront+urlencode(params, quote_via=quote_plus)
-    answer = json.loads(requests.get(url).text)
+    altUrl = altUrlFront+stdSearch+altUrlBack
+    try:
+        answer = json.loads(requests.get(url).text)
+    except:
+        print("Please connect to a network.")
+        sys.exit()
+
     if(answer['Answer'] != '' and answer['Answer']['id'] == 'calculator'):
         exp = str(parser.parse(search).evaluate({}))
         print('{} = {}'.format(search, exp))
@@ -39,3 +52,6 @@ else:
         print('Related topics:')
         for i in range(0, counter):
             print('\t{}\n\t{}\n'.format(text[i], urls[i]))
+
+        if(counter == 0):
+            print("\nNo instant answers available, here's a direct link:\n\t{}\n".format(altUrl))
